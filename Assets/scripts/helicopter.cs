fad3d9a6e4c4a94158aecd3beb8dc28c7a0d7592
mpacdia <146739066+mpacdia@users.noreply.github.com>
2024-03-30 12:59:51 +0000

    vcerfger

---
+++ Assets/scripts/helicopter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helicopter : MonoBehaviour
+{
+    public gameObject empty;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
